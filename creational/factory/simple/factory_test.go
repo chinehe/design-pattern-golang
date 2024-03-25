b98a9c78fc9bb88b4696a443bcef5a35fc6a3e25
qing.he <qing.he@cardinfolink.com>
2024-03-25 16:59:22 +0800

    简单工厂模式

--- creational/factory/simple/factory_test.go
+++ creational/factory/simple/factory_test.go
@@ -4,9 +4,7 @@ import (
 	"testing"
 )
 
-func TestNewIRuleConfigParser(t *testing.T) {
-	jsonParser := NewIRuleConfigParser("json")
-	yamlParser := NewIRuleConfigParser("yaml")
-	jsonParser.Parse([]byte("json"))
-	yamlParser.Parse([]byte("yaml"))
+func TestNewLogger(t *testing.T) {
+	NewLogger("Error").Println("Hello")
+	NewLogger("Info").Println("Hello")
 }
